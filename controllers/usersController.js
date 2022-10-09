0841fb1fdb17a8df8768aa09d019094ffa022423
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2022-10-09 03:20:12 -0500

    MVC users
    Agregado modelo vista y controlador de usuarios.
    
    Se agrega módulo de registro, login y búsqueda de usuarios por id, email y verificación de contraseñas cifradas.

---
+++ controllers/usersController.js
@@ -0,0 +1,97 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const keys = require("../config/keys");
+
+module.exports = {
+  //petición asíncrona para todos los usuarios
+  async getAll(req, res, next) {
+    try {
+      const data = await User.getAll();
+      console.log(`Usuarios: ${data}`);
+      return res.status(201).json(data);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return res.status(501).json({
+        success: false,
+        message: "Error al obtener todos los usuarios\n" + error,
+      });
+    }
+  },
+  //petición asíncrona para insertar nuevos usuarios
+  async register(req, res, next) {
+    try {
+      const user = req.body;
+      //se espera a que se termine el  proceso
+      const data = await User.create(user);
+      return res.status(201).json({
+        success: true,
+        message: "Registro realizado. Inicia sesión.",
+        data: data.session_token,
+      });
+    } catch (error) {
+      console.log(`${error}`);
+      return res.status(501).json({
+        success: false,
+        message: "Error con el registro+\n" + error,
+        error: error,
+      });
+    }
+  },
+  async login(req, res, next) {
+    try {
+      //Se recuperan las variables del body de la request
+      const email = req.body.email;
+      const password = req.body.password;
+      //Se busca el usuario por el email recibido
+      const rUser = await User.FindByEmail(email);
+      //si no hay usuario con ese email
+      if (!rUser) {
+        return res.status(401).json({
+          success: false,
+          message: "El email no fue encontrado",
+          error: error,
+        });
+      }
+
+      //si la contraseña enviada por el usuario es igual a la cifrada en DB
+      if (User.isPwMatched(password, rUser.password)) {
+        const token = jwt.sign(
+          { id: rUser.id, email: rUser.email },
+          //Token de sesión se le puede asignar expiración
+          keys.secretOrKey,
+          {
+            //Duración de una hora
+            //expiresIn: 60 * 60 * 24,
+          }
+        );
+        const data = {
+          id: rUser.id,
+          name: rUser.name,
+          lastname: rUser.lastname,
+          email: rUser.email,
+          phone: rUser.phone,
+          image: rUser.image,
+          session_token: `JWT ${token}`,
+        };
+
+        return res.status(201).json({
+          success: true,
+          data: data,
+          message: "Ingreso correcto",
+        });
+      } else {
+        return res.status(401).json({
+          success: false,
+          message: "La contraseña es incorrecta",
+        });
+      }
+    } catch (error) {
+      console.log(`Error login. ${error}`);
+      return res.status(501).json({
+        success: false,
+        message: "Error al hacer login",
+        error: error,
+      });
+    }
+  },
+};
---
+++ models/user.js
@@ -0,0 +1,94 @@
+//recuperar el módulo db desde config para sentencias SQL
+const db = require("../config/config");
+//recuperar dependencia para encriptar las contraseñas
+const crypto = require("crypto");
+
+const User = {};
+
+//sentencia SQL que recupera todos los usurios
+User.getAll = () => {
+  const sql = `
+    SELECT 
+    * 
+    FROM 
+    users`;
+
+  return db.manyOrNone(sql);
+};
+
+//sentencia SQL que recupera un único usuario por I_D
+User.findById = (id, callback) => {
+  const sql = `
+    SELECT
+      id,
+      email,
+      name,
+      lastname,
+      password,
+      session_token
+    FROM
+        users
+    WHERE
+        id = $1`;
+
+  return db.oneOrNone(sql, id).then((user) => {
+    callback(null, user);
+  });
+};
+
+//Sentencia SQL que recupera un único usuario por email
+User.FindByEmail = (email) => {
+  const sql = `
+      SELECT
+        U.id,
+        U.email,
+        U.name,
+        U.lastname,
+        U.password,
+        U.session_token,
+      FROM
+        users;    
+    `;
+
+  return db.oneOrNone(sql, email);
+};
+
+//sentencia SQL que crea nuevo usuario
+User.create = (user) => {
+  //se encripta en md5, update(pasamos el valor) digest para mantener formato hex
+  const pwHash = crypto.createHash("md5").update(user.password).digest("hex");
+
+  user.password = pwHash;
+  const sql = `
+      INSERT INTO
+        users(
+          email,
+          name,
+          lastname,
+          password,
+          created_at,
+          updated_at
+        )
+      VALUES($1, $2, $3, $4, $5, $6) RETURNING id
+    `;
+  return db.oneOrNone(sql, [
+    user.email,
+    user.name,
+    user.lastname,
+    user.password,
+    new Date(),
+    new Date(),
+  ]);
+};
+
+//Comparar una contraseña enviada con pw encriptado en DB
+User.isPwMatched = (uPW, hash) => {
+  const pwHashed = crypto.createHash("md5").update(uPW).digest("hex");
+  if (pwHashed === hash) {
+    return true;
+  }
+  return false;
+};
+
+//objeto para el controlador
+module.exports = User;
