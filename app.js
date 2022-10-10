27326624f83ab6fd4e405035529f3824171b8cc5
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2022-10-10 01:36:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,9 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers, Authorization",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Methods",
+    "GET,POST,OPTIONS,DELETE,PUT"
   );
   next();
 });
