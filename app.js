035fb6d415bb318a35faafacafc2ae5bfa7b3614
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2022-10-10 01:37:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const users = require("./routes/usersRoutes");
-const User = require("../models/user");
+const User = require("./models/user");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
