e594baab8ae064899feb71b95ab67d9a9ed4fa07
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2022-10-10 21:25:36 -0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,9 @@ const Keys = require("./keys");
 module.exports = function (passport) {
   let opts = {};
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-  opts.secretOrKey = process.env.secretOrKey;
+  opts.secretOrKey =
+    process.env.secretOrKey ||
+    "w!z%C*F-J@NcRfUjXn2r5u8x/A?D(G+KbPdSgVkYp3s6v9y$B&E)H@McQfThWmZq";
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
       User.findById(jwt_payload.id, (err, user) => {
