c4f73b43becfe1b4ead6a14f0d8bf014452270d9
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2022-10-17 14:07:43 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -329,7 +329,7 @@ User.PostEquipo = (equipamiento) => {
             "Disponibilidad",
             "Partida"
         )
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) returning
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,  $11) returning idequipo
     `;
   return db.oneOrNone(sql, [
     equipamiento.nombre,
