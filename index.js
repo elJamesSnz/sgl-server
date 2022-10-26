c9f6ef979bde77afba24a2c9df03c0fa0ca10c53
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2022-10-26 12:14:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require("express");
+const session = require("express-session");
 const cors = require("cors");
+const logger = require("morgan");
+const http = require("http");
 
 const users = require("./routes/usersRoutes");
 const hellos = require("./routes/hello");
+const port = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(cors({ origin: "*" }));
 app.use(express.json());
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "-",
+  })
+);
+
+app.use(logger("dev"));
+
+app.disable("x-powered-by");
+
+app.set("port", port);
 
 users(app);
 hellos(app);
 
-app.listen(process.env.port, () => {
-  console.log("Servidor iniciado");
+const server = http.createServer(app);
+server.listen(port, "192.168.47.1" || "localhost", () => {
+  console.log("Servidor iniciado en puerto: " + port);
 });
