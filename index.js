2aae2dc43699fac1af83df160e874178acb0bd92
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2022-10-26 12:18:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers, Authorization",
+    "Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Methods",
+    "GET,POST,OPTIONS,DELETE,PUT"
+  );
+  next();
+});
+
 app.use(cors({ origin: "*" }));
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(
   session({
